c997c8fc74ec041b9bd83434e42bed039ec44d7a
J-IgorSilva <aleatoria8821@gmail.com>
2022-06-25 23:39:36 -0300

    Projeto Consumo de API externa usando lib REFIT .NET

---
+++ CepApi/CepApi/CepResponse.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CepApi
+{
+    public class CepResponse
+    {
+        [JsonProperty("cep")]
+        public string Cep { get; set; }
+
+        [JsonProperty("logradouro")]
+        public string Logradouro { get; set; }
+
+        [JsonProperty("complemento")]
+        public string Complemento { get; set; }
+
+        [JsonProperty("bairro")]
+        public string Bairro { get; set; }
+
+        [JsonProperty("localidade")]
+        public string Localidade { get; set; }
+
+        [JsonProperty("uf")]
+        public string Uf { get; set; }
+
+        [JsonProperty("ibge")]
+        public string Ibge { get; set; }
+
+        [JsonProperty("gia")]
+
+        public string Gia { get; set; }
+
+        [JsonProperty("ddd")]
+        public int DDD { get; set; }
+
+        [JsonProperty("siafi")]
+        public int Siafi { get; set; }
+    }
+}
---
+++ CepApi/CepApi/Program.cs
@@ -0,0 +1,34 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace CepApi
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepCliente = RestService.For<ICepApiService>("http://viacep.com.br/");
+
+                Console.WriteLine("Informe Seu CEP");
+
+                var cepInformado = Console.ReadLine().ToString();
+
+                Console.WriteLine("Consultando CEP informado" + cepInformado);
+
+                var address = await cepCliente.GetAddressAsync(cepInformado);
+
+                Console.Write($"\nLogradouro:{address.Logradouro},\nBairro{address.Bairro},\nEstado{address.Uf}");
+
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Cep não Encontrado" + e.Message);
+            }
+        }
+    }
+}
